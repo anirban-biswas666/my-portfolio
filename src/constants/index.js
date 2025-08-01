30a1893a425303715ca6adf27cc0c9e1e9b7fec7
anirban-biswas666 <120649756+anirban-biswas666@users.noreply.github.com>
2025-08-01 22:13:50 +0530

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,264 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Crypto flow",
+    description:
+      "This is a powerful crypto tracking app that provides real-time market data, including 24-hour price changes and total market values of all major cryptocurrencies.",
+    subDescription: [
+      "Built a scalable application with REACT and using VITE js to deliver up-to-date market data, including real-time values.",
+         "The app also displays the previous 8 days of market history with interactive and visually appealing charts powered by Google Charts, helping users make informed decisions with ease.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/Cryptoflow.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Vite",
+        path: "/assets/logos/.svg",
+      },
+      
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Forest App (Focus Timer)",
+    description:
+      "Forest App is a minimalist, Pomodoro-style focus timer designed to help users boost productivity and maintain deep focus during work sessions. The clean UI and intuitive controls provide a distraction-free experience.",
+    subDescription: [
+      "⏱️ Pomodoro-based timer for structured work and break intervals",
+      "🛡️ Developed with TypeScript for robust type safety and improved code reliability",
+      "🎨 Designed with TailwindCSS for a responsive, mobile-friendly layout",
+      "💡 Simple, distraction-free interface to support focused work sessions",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/ForestApp.png",
+    tags: [
+      {
+        id: 1,
+        name: "Auth0",
+        path: "/assets/logos/Typescript.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Snap.ai-Saas",
+    description:
+      "Snap.ai is a powerful AI-driven SaaS platform designed to help users streamline content creation and personal productivity. Whether you’re writing articles, generating images, removing backgrounds, or reviewing resumes—Snap.ai puts smart tools at your fingertips.",
+    subDescription: [
+      "AI Article & Blog Generator – Instantly create high-quality content using OpenAI",
+      " Image Tools – Generate images and remove backgrounds/objects with Clipdrop API",
+      " Resume Reviewer – Get intelligent feedback and improvement suggestions using AI",
+      "Secure Auth – Seamless sign-in and account management with Clerk",
+      "Responsive UI – Beautifully designed in Figma and styled with TailwindCSS for a smooth experience on all devices",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/Snap.ai-Saas.png",
+    tags: [
+      {
+        id: 1,
+        name: "Postgresql",
+        path: "/assets/logos/postgresql.svg",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "Expressjs",
+        path: "/assets/logos/Express.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+       {
+        id: 5,
+        name: "Nodejs",
+        path: "/assets/logos/Node.js.svg",
+      },
+      {
+        id: 6,
+        name: "Reactjs",
+        path: "/assets/logos/react.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "ToDo-List App",
+    description:
+      "The To-Do List App is a simple yet powerful task management tool that allows users to stay organized and productive. With user authentication and full CRUD functionality, it offers a seamless experience for managing daily tasks.",
+    subDescription: [
+      " Secure login and signup system to manage personal task lists",
+      " Task Management (CRUD) – Create, read, update, and delete tasks easily",
+      "Full Stack Deployment – Hosted on Vercel, backed by MongoDB for persistent storage",
+      " Clean & Intuitive UI – Built with HTML, CSS, and JavaScript for a lightweight, responsive interface",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/To-Do List.png",
+    tags: [
+      {
+        id: 1,
+        name: "html",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "css",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+       {
+        id: 5,
+        name: "MongoDB",
+        path: "/assets/logos/MongoDB.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "TicTacToe",
+    description:
+      "This AI-powered Tic-Tac-Toe game offers a fun and challenging experience with an unbeatable computer opponent. Designed for smooth interaction and clean visuals, it’s a modern twist on a classic game using smart algorithms and intuitive UI.",
+    subDescription: [
+      "Unbeatable AI – Implements the Min-Max Algorithm for perfect decision-making",
+      " UI Designed in Figma – Clean and engaging layout for a smooth user experience",
+      " React-Powered Gameplay – Uses state management for real-time interactivity and responsiveness",
+      " Single-Player Challenge – Go head-to-head against a smart AI in every match",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/TicTacToe.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Figma",
+        path: "/assets/logos/Figma.svg",
+      },
+    ],
+  },
+  
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
